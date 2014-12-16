ea34c55615269422760b0d4dac598246c55ceb5c
aquilax <evgeniy@wikia-inc.com>
2014-12-16 05:19:56 +0100

    Changed front page ordering

--- listboard.go
+++ listboard.go
@@ -81,7 +81,7 @@ func (l *Listboard) indexHandler(w http.ResponseWriter, r *http.Request) {
 	sc := l.config.getSiteConfig(l.getToken(r))
 	s := NewSession(sc, l.tp.Get(sc.Language))
 	s.AddPath("", s.Lang("Home"))
-	s.Set("Lists", l.m.mustGetChildNodes(sc.DomainId, 0, itemsPerPage, page, "updated"))
+	s.Set("Lists", l.m.mustGetChildNodes(sc.DomainId, 0, itemsPerPage, page, "updated DESC"))
 	s.render(w, r, "templates/layout.html", "templates/index.html")
 }
 
